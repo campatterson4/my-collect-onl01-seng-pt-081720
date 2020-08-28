4ad904e67a99ea25cea195d8c2736ab9ff35bb2b
Camryn Patterson <camrynpatterson@yahoo.com>
2020-08-28 04:09:26 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+list = ["The Office", "Game of Thrones", "Breaking Bad"]
 
+
+def my_collect(array)
+  i = 0
+  name_collection = []
+  while i < array.length
+    name_collection.push yield(array[i])
+  << yield(array[i])
+    i += 1
+  end
+  name_collection
+end
+
+my_collect(list) {|i| i.split(" ").first}
 
